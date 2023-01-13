9df635a5ca9a9d3fc5877099a517fc1dfac0e1f2
techdrivetesting <91088563+techdrivetesting@users.noreply.github.com>
2023-01-12 20:08:37 -0500

    vpc_id

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ module "blog_sg" {
   version = "4.16.2"
   
   name                = "blog_new"
-  vpc_id              = module.blog_vpc.id
+  vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
