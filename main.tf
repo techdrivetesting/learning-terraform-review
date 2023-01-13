58c1aeddba95e035eea355db3c1f3f9cffbba2d8
techdrivetesting <91088563+techdrivetesting@users.noreply.github.com>
2023-01-12 19:37:48 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -31,19 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.0"
+  
+  name     = "blog" 
+  miz_size = 1
+  max_size = 2
+
+  vpc_zone_indentifier = module.blog_vpc.public_subnets
+  target_group_arns    = module.blog_alb.target_group_arns
+  security_groups      = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
-  tags = {
-    Name = "Learning Terraform Review"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -54,7 +58,6 @@ module "alb" {
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
 
   target_groups = [
     {
