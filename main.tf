5a41f20e106628d8cdc4d12589ce662e5898d421
techdrivetesting <91088563+techdrivetesting@users.noreply.github.com>
2023-01-12 20:00:23 -0500

    bracket

--- main.tf
+++ main.tf
@@ -86,11 +86,10 @@ module "blog_sg" {
   version = "4.16.2"
   
   name    = "blog_new"
-  vpc_id  = module.blog_vpc.public_subnets["0", "1", "2", "3"]
+  vpc_id  = module.blog_vpc.public_subnets["0", "1", "2", "3"] 
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
