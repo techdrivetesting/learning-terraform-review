2a5059e4b282961407c994c87d1f1249abe85501
techdrivetesting <91088563+techdrivetesting@users.noreply.github.com>
2023-01-12 20:07:10 -0500

    another cor

--- main.tf
+++ main.tf
@@ -85,9 +85,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
   
-  name    = "blog_new"
-  vpc_id  = module.blog_vpc.public_subnets["3"] 
-
+  name                = "blog_new"
+  vpc_id              = module.blog_vpc.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
