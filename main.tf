0b2f80ba774b9267762a12de1737055d522de86c
techdrivetesting <91088563+techdrivetesting@users.noreply.github.com>
2023-01-12 19:16:55 -0500

    sg vpc name correction

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
   
   subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_vpc.security_group_id]
   
   tags = {
     Name = "Learning Terraform Review"
